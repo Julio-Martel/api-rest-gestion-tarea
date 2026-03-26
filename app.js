97d66ec50ce79be968cfde16351a8ec46129679a
Julio-Martel <julioramdom123@gmail.com>
2026-03-26 18:00:48 -0300

    feat: agrega estructura de la aplicacion

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const usuariosRoutes = require('./routes/usuarios.routes');
+const taresRoutes = require('./routes/tareas.routes');
+
+app.use(express.json());
+
+
+app.use('/usuarios', usuariosRoutes);
+app.use('/tareas', taresRoutes);
+
+app.listen(3000,() => {
+    console.log('Servidor activo!');
+})
