dfba9e3b1003d278ed0402da8162f5f5cf49446f
Julio-Martel <julioramdom123@gmail.com>
2026-03-28 18:06:14 -0300

    feat: agrega login

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,8 @@
+const usuarios = require('../database/usuarios');
+const utils = require('../utils/utils');
+
+const validarUsuario = (req, res, next) => {
+    const {usuario, password} = req.body;
+
+
+}
---
+++ middlewares/permisos.middleware.js
@@ -0,0 +1,34 @@
+const usuarios = require('../database/usuarios');
+const utils = require('../utils/utils')
+
+
+const loginUsuario = (req, res, next) => {
+    
+    if(!req.body || utils.validarBody(req.body)){
+        return res.status(403).json({
+            mensaje: 'Debe rellenar la casilla body'
+        })
+    } 
+
+    const {usuario,password} = req.body;
+
+    if(!usuario || !password){
+        return res.status(403).json({
+            mensaje: 'Los campos deben estar llenos'
+        })
+    }
+
+    const usuarioExistente = usuarios.find(u => u.usuario === usuario && u.password === password)
+
+    if(usuarioExistente === undefined){
+        return res.status(403).json({
+            mensaje: 'Usuario no encontrado'
+        })
+    }
+
+    next();
+
+}
+
+
+module.exports = {loginUsuario};
