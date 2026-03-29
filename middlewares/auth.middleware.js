e979a5ef53fe67e914c46af362dfc80f6ea2b8d5
Julio-Martel <julioramdom123@gmail.com>
2026-03-29 13:12:27 -0300

    delete: utils

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,17 +1,17 @@
 const usuarios = require('../database/usuarios');
-const utils = require('../utils/utils');
 
 const validarUsuarioPorRol = (req, res, next) => {
     const {usuario, password} = req.body;
 
     const usuarioFiltrado = usuarios.find(u => u.usuario === usuario && u.password === password)
 
-    if(usuarioFiltrado.rol === 'admin'){
+    if(usuarioFiltrado.rol !== 'admin'){
         return res.status(200).json({
-            mensaje: 'Puede realizar cambios en la base de datos de usuarios'
+            mensaje: 'Necesita permisos de administrador para poder acceder a la base de datos'
         })
     }
 
+    next();
 }
 
 module.exports = {
