30b3d3bb40052d008318667c33ab1464b0973f97
Julio-Martel <julioramdom123@gmail.com>
2026-03-29 14:34:01 -0300

    feat: borrar usuario

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -8,5 +8,6 @@ const authMiddlewares = require('../middlewares/auth.middleware');
 router.post('/registro', usuarioController.registrarUsuario);
 router.post('/login', middlewares.loginUsuario,authMiddlewares.validarUsuarioPorRol);
 router.get('/',middlewares.loginUsuario,authMiddlewares.validarUsuarioPorRol, usuarioController.mostrarTodosLosUsuarios);
+router.delete('/',middlewares.loginUsuario,authMiddlewares.validarUsuarioPorRol, usuarioController.eliminarUsuario);
 
 module.exports = router;
