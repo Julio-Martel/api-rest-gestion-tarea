e62f4b5998cc50de724c8947125b29a6c652f140
Julio-Martel <julioramdom123@gmail.com>
2026-03-29 14:02:30 -0300

    feat: agrega ruta obtener usuarios

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const usuarioController = require('../controllers/usuarios.controllers')
+const usuarioController = require('../controllers/usuarios.controllers');
+const middlewares = require('../middlewares/permisos.middleware');
+const authMiddlewares = require('../middlewares/auth.middleware');
 
-router.post('/', usuarioController);
+router.post('/registro', usuarioController.registrarUsuario);
+router.post('/login', middlewares.loginUsuario,authMiddlewares.validarUsuarioPorRol);
+router.get('/',middlewares.loginUsuario,authMiddlewares.validarUsuarioPorRol, usuarioController.mostrarTodosLosUsuarios);
 
 module.exports = router;
