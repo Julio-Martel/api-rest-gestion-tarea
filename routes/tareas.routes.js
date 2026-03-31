dedbae88b2d564ae444cb4254dac31db4fad7493
Julio-Martel <julioramdom123@gmail.com>
2026-03-31 19:42:13 -0300

    feat: agrega ruta

--- routes/tareas.routes.js
+++ routes/tareas.routes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 const middleware = require('../middlewares/permisos.middleware');
 const tareaControllers = require('../controllers/tareas.controllers');
+const authMiddlewares = require('../middlewares/auth.middleware')
 
 router.post('/', middleware.loginUsuario, tareaControllers.crearTarea);
 router.get('/', middleware.loginUsuario, tareaControllers.validarPermiso);
+router.delete('/', middleware.loginUsuario, authMiddlewares.validarUsuarioPorRol, tareaControllers.eliminarTarea);
+
 
 module.exports = router;
 
