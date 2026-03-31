0c3a7e5e1144ec8f9260afbe00675729e7bfd872
Julio-Martel <julioramdom123@gmail.com>
2026-03-31 19:47:28 -0300

    refactor: agrega mas tareas

---
+++ database/tareas.js
@@ -0,0 +1,73 @@
+const tareas = [
+  {
+    id: 1,
+    titulo: "Aprender Node",
+    descripcion: "Practicar API REST",
+    estado: "pendiente",
+    usuarioId: 1
+  },
+  {
+    id: 2,
+    titulo: "Hacer ejercicio",
+    descripcion: "Salir a correr 30 minutos",
+    estado: "en progreso",
+    usuarioId: 1
+  },
+  {
+    id: 3,
+    titulo: "Estudiar matemáticas",
+    descripcion: "Repasar integrales",
+    estado: "pendiente",
+    usuarioId: 2
+  },
+  {
+    id: 4,
+    titulo: "Leer libro",
+    descripcion: "Terminar capítulo 5",
+    estado: "completado",
+    usuarioId: 2
+  },
+  {
+    id: 5,
+    titulo: "Practicar backend",
+    descripcion: "Hacer middlewares",
+    estado: "en progreso",
+    usuarioId: 1
+  },
+  {
+    id: 6,
+    titulo: "Limpiar habitación",
+    descripcion: "Ordenar escritorio",
+    estado: "pendiente",
+    usuarioId: 2
+  },
+  {
+    id: 7,
+    titulo: "Ver curso de JavaScript",
+    descripcion: "Promesas y async/await",
+    estado: "completado",
+    usuarioId: 1
+  },
+  {
+    id: 8,
+    titulo: "Cocinar",
+    descripcion: "Preparar cena saludable",
+    estado: "pendiente",
+    usuarioId: 2
+  },
+  {
+    id: 9,
+    titulo: "Revisar código",
+    descripcion: "Refactorizar proyecto",
+    estado: "en progreso",
+    usuarioId: 1
+  },
+  {
+    id: 10,
+    titulo: "Dormir temprano",
+    descripcion: "Acostarse antes de las 23",
+    estado: "pendiente",
+    usuarioId: 2
+  }
+];
+module.exports = tareas;
---
+++ database/usuarios.js
@@ -0,0 +1,16 @@
+const usuarios = [
+  {
+    id: 1,
+    usuario: "julioM",
+    password: "1234",
+    rol: "admin"
+  },
+  {
+    id: 2,
+    usuario: "yuki",
+    password: "abcd",
+    rol: "user"
+  }    
+];
+
+module.exports = usuarios;
